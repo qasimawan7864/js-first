d7b2726ea399619c2fa9a878b924c7b7c1da9984
Qasim Awan <qasimawanpk1@gmail.com>
2026-04-29 11:36:57 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,58 @@
+"use stict"; // treat all js code as newer version
+
+// alert (3+3) // we are using node js, not browser
+
+console.log(3 + 3) // code readability shhould be high 
+
+// ===============================
+// Primitive DataTypes (Simple Notes + Output)
+// ===============================
+
+// 1) STRING   => text / words ہمیشہ " " یا ' ' کے اندر لکھتے ہیں
+let name = "Qasim Awan";
+console.log("Name:", name);
+console.log("Type:", typeof name);
+
+
+// 2) NUMBER (decimal+integer)  => عام numbers (limit: 2^53 - 1)
+let age = 25;
+let price = 199.99;
+console.log("Age:", age);
+console.log("Price:", price);
+console.log("Type:", typeof age);
+
+
+// 3) BIGINT  => بہت بڑے numbers (end with 'n')
+let bigNumber = 123456789123456789123456789n;
+console.log("Big Number:", bigNumber);
+console.log("Type:", typeof bigNumber);
+
+
+// 4) BOOLEAN   => true / false
+let isLoggedIn = true;
+let isAdmin = false;
+console.log("Is Logged In:", isLoggedIn);
+console.log("Is Admin:", isAdmin);
+console.log("Type:", typeof isLoggedIn);
+
+
+// 5) NULL  => empty value (intentionally assigned)
+let state = null;
+console.log("State:", state);
+console.log("Type:", typeof state); // ⚠️ object آتا ہے (JavaScript bug)
+
+
+// 6) UNDEFINED  => value assign نہیں کی گئی
+let city;
+console.log("City:", city);
+console.log("Type:", typeof city);
+
+
+// 7) SYMBOL  => unique value
+let id1 = Symbol("id");
+let id2 = Symbol("id");
+console.log("ID1:", id1);
+console.log("ID2:", id2);
+console.log("Are equal?", id1 === id2); // false
+console.log("Type:", typeof id1);
+
